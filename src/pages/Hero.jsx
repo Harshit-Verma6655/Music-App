88d85dd1b179833417d1dcb7c5ad145f443e9048
Harshit-Verma6655 <144591839+Harshit-Verma6655@users.noreply.github.com>
2024-01-27 23:15:09 +0530

    load more

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -24,15 +24,16 @@ useEffect(  ()=>{
 
 
     <>
-    <div className='flex flex-col items-center justify-center mt-48 sm:mt-24  absolute bg-gray-900 w-full h-screen box-border '>
+    <div className='flex flex-col items-center justify-center mt-48 sm:mt-24   bg-gray-900 w-full h-screen box-border pb-20'>
     <div className='bg-gray-900 w-full  box-border p-2 flex justify-center  flex-wrap overflow-scroll overflow-x-hidden  hero   '   >
         {musicList?.map(obj=><Card key={obj._id} obj={obj}/>)}
 
-        
+        <div className='w-full text-center'><button className='bg-green-500 z-20 text-white px-1 rounded mt-1  w-fit text-xs text-center sticky ' onClick={()=>setPage(++page)}>Load More</button></div>
     </div>
-    <button className='bg-green-500 z-20 text-white px-1 rounded mt-1  w-fit text-xs text-center' onClick={()=>setPage(++page)}>Load More</button>
+   
+    
+    </div>
     {footer && <Footer/>}
-    </div>
    
     
     
